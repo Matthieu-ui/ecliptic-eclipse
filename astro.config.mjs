9ae804c4a798a3a331458dc4242534458d02c7e7
Matthieu-ui <mattfelker206@outlook.com>
2023-05-25 18:50:23 -0700

    csgo blog init working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+  integrations: [tailwind(), react()]
+});
